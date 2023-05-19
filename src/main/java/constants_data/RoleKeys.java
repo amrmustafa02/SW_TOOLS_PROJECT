e412b2cb97026b182250110758b00a94a784bfa0
youssefsharkawy <youssefsharkawy22@gmail.com>
2023-05-20 02:40:05 +0300

    add roles keys

---
+++ src/main/java/constants_data/RoleKeys.java
@@ -0,0 +1,9 @@
+package constants_data;
+
+
+public class RoleKeys {
+    public static String RestaurantOwner = "restaurant owner";
+    public static String CustomerOwner = "Customer owner";
+    public static String RunnerOwner = "Runner owner";
+
+}
