02273dedeb180f78261d79ed82446d2e5c807eda
amrmustafa02 <amrmustafa6051@gmail.com>
2023-05-19 23:51:35 +0300

    start implement order entity

--- src/main/java/com/redhat/model/Order.java
+++ src/main/java/com/redhat/model/Order.java
@@ -1,4 +1,39 @@
 package com.redhat.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    double totalPrice;
+    String orderStatus;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
 }
