0bfb3566fab2b190d490370a0883151a7eb04d09
amrmustafa02 <amrmustafa6051@gmail.com>
2023-05-20 01:39:14 +0300

    update war file

---
+++ target/generated-sources/annotations/com/redhat/model/Order_.java
@@ -0,0 +1,16 @@
+package com.redhat.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Order.class)
+public abstract class Order_ {
+
+	public static volatile SingularAttribute<Order, Double> totalPrice;
+	public static volatile SingularAttribute<Order, String> orderStatus;
+	public static volatile SingularAttribute<Order, Integer> id;
+
+}
+
